6f962a4dbff3f04f2d6042ca9de1a1821aa53ed0
mhsamie <sfatima7720@gmail.com>
2022-07-18 16:58:20 +0430

    search section added

--- src/components/transActionComponent.js
+++ src/components/transActionComponent.js
@@ -1,18 +1,47 @@
+import { useState, useEffect } from "react";
+
 const TransAction = ({ transAction }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredItem, setFilteredItem] = useState(transAction);
+  const filterItems = (search) => {
+    if (!search) {
+      setFilteredItem(transAction);
+      return;
+    }
+    const filteredTransaction = transAction.filter((t) =>
+      t.description.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredItem(filteredTransaction);
+  };
+  const searchHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterItems(e.target.value);
+  };
+  useEffect(() => {
+    filterItems(searchItem);
+  }, [transAction]);
   return (
     <section>
-      {transAction.length &&
-        transAction.map((t) => (
-          <div
-            key={t.id}
-            className={`bg-gray-200 border-r-4 border-solid m-2 p-2 rounded-lg flex justify-between ${
-              t.type === "Expense" ? "border-red-600" : "border-green-600"
-            }`}
-          >
-            <span>{t.description}</span>
-            <span>{t.amount} $</span>
-          </div>
-        ))}
+      <input
+        placeholder="search the transactions"
+        type="text"
+        value={searchItem}
+        onChange={searchHandler}
+        className="border w-60 mt-2 border-solid border-gray-200 rounded bg-gray-100"
+      />
+      {filteredItem.length
+        ? filteredItem.map((t) => (
+            <div
+              key={t.id}
+              className={`bg-gray-200 border-r-4 border-solid m-2 p-2 rounded-lg flex justify-between ${
+                t.type === "Expense" ? "border-red-600" : "border-green-600"
+              }`}
+            >
+              <span>{t.description}</span>
+              <span>{t.amount} $</span>
+            </div>
+          ))
+        : "add transactions"}
     </section>
   );
 };
